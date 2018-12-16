304f30935ccae5bac06b6940fd11a1570b505f99
Adam Monahan <adammonahan93@gmail.com>
2018-12-15 21:20:39 -0800

    runs faster and handles longs

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # C-projects
+nthp.c
+compile with gcc nthp.c -o nthp
+Prints the nth prime number, where n is the first command line argument
+(behavior is not defined for n < 1 or non-integer arguments)
+
+Example session:
+./nthp 1
+2
+./nthp 2
+3
+./nthp 100
+541
+./nthp 10000000
+179424673
--- nthp.c
+++ nthp.c
@@ -2,21 +2,21 @@
 #include <stdio.h>
 #include <math.h>
 
-#define MAXLISTSIZE 1500000
+#define MAXLISTSIZE 1000000000
 #define MIN(X, Y) (((X) < (Y)) ? (X) : (Y))
 
 int main(int argc, char** argv){
 //Calculate the nth prime number. n = newargv[1]
-int i = 1; 
-int j;
 int isPrime;
 int n = atoi(argv[1]);
-int prime = 0;
-int nextprime;
-int upto;
 int upperbound = MIN(n, MAXLISTSIZE);
-int primes[upperbound];
-int *nthprime = &primes[upperbound - 1];
+int prime = 0;
+int upto;
+int i = 1; 
+int j;
+int nextprime;
+int *primes = malloc(sizeof(int) * upperbound);
+long nthprime;
 //Define first prime number as 2
 primes[prime++] = 2;
 while (prime < upperbound){
@@ -41,18 +41,20 @@ while (prime < upperbound){
 		primes[prime++] = i;
 	}
 }
+nthprime = (long) primes[upperbound - 1];
+long iLong = (long) i;
 n -= upperbound;
 //Continue if we're out of list space but don't know the nth prime
 //yet
 while (n  > 0){
-	i += 2; 
-	upto = (int) sqrt(i);
+	iLong += 2; 
+	upto = (int) sqrt(iLong);
 	isPrime = 1;
 	//Check the numbers on the list first
 	for (j = 1; j < upperbound; j++){
 		nextprime = primes[j];
 		if (nextprime > upto) break;
-		if (i % nextprime == 0){
+		if (iLong % nextprime == 0){
 			isPrime = 0;
 			break;
 		}
@@ -60,7 +62,7 @@ while (n  > 0){
 	//If it still might be prime, keep checking odd numbers
 	if (isPrime) for (j = primes[upperbound - 1] + 2; j <= upto; 
 			  j += 2){
-		if (i % j == 0){
+		if (iLong % j == 0){
 			isPrime = 0;
 			break;
 		}	
@@ -68,10 +70,11 @@ while (n  > 0){
 	//No space to save primes, so use n to keep track of which
 	//prime we're on
 	if (isPrime){
-		*nthprime = i;
+		nthprime = iLong;
 		n--;
 	}
 }
 //print the n'th prime
-printf("%d\n", *nthprime);
+printf("%ld\n", nthprime);
+free(primes);
 }
